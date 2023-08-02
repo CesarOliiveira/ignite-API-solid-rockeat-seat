9336f9d62e886e4f2651cfbaa241680752b5e18e
CesarOliiveira <cesaroliiveira8@gmail.com>
2023-08-02 13:56:31 -0300

    Separating application files and adding Docker.

---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { env } from '@/env';
+import { PrismaClient } from '@prisma/client';
+
+
+
+const prisma = new PrismaClient({
+    log: env.NODE_ENV == 'dev' ? ['query'] : [],
+});
+
+export {prisma};
