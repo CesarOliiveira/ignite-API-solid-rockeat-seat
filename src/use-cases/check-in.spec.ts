66e906494ab5f7b3458bba1dacc445c260204781
CesarOliiveira <cesaroliiveira8@gmail.com>
2023-10-06 20:02:59 -0300

    Setting the check-in radius to 100 meters.

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -19,8 +19,8 @@ describe('Check-in Use Case', () => {
             title: 'JavaScript Gym',
             description: '',
             phone: '',
-            latitude: new Decimal(0),
-            longitude: new Decimal(0),
+            latitude: new Decimal(-27.2092052),
+            longitude: new Decimal(-49.6401091),
         })
 
         vi.useFakeTimers()
@@ -35,8 +35,8 @@ describe('Check-in Use Case', () => {
         const { checkIn } = await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
-            userLatitude: 0,
-            userLongitude: 0,
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
         })
 
 
@@ -50,15 +50,15 @@ describe('Check-in Use Case', () => {
         await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
-            userLatitude: 0,
-            userLongitude: 0,
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
         })
 
        await expect(() => sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
-            userLatitude: 0,
-            userLongitude: 0,
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
        })).rejects.toBeInstanceOf(Error)
     })
 
@@ -69,8 +69,8 @@ describe('Check-in Use Case', () => {
         await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
-            userLatitude: 0,
-            userLongitude: 0,
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
         })
 
         vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
@@ -78,11 +78,30 @@ describe('Check-in Use Case', () => {
         const {checkIn} = await sut.execute({
             gymId: 'gym-01',
             userId: 'user-01',
-            userLatitude: 0,
-            userLongitude: 0,
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
         })
 
         expect(checkIn.id).toEqual(expect.any(String))
+    })
+
+    it('should not be able to check in on distant gym', async () => {
+       
+        gymsRepository.items.push({
+            id: 'gym-02',
+            title: 'JavaScript Gym',
+            description: '',
+            phone: '',
+            latitude: new Decimal(-27.0747279),
+            longitude: new Decimal(-49.4889672),
+        })
+
+        expect(() => sut.execute({
+            gymId: 'gym-02',
+            userId: 'user-01',
+            userLatitude: -27.2092052,
+            userLongitude: -49.6401091,
+        })).rejects.toBeInstanceOf(Error)
     })
 
 
--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -2,6 +2,7 @@ import { GymsRepository } from './../repositories/gyms.repository';
 import { CheckIn } from '@prisma/client';
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates';
 
 
 interface CheckInUseCaseRequest {
@@ -24,6 +25,8 @@ export class CheckInUseCase {
     async execute({
         userId,
         gymId,
+        userLatitude,
+        userLongitude
     }: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
         const gym = await this.gymsRepository.findById(gymId)
 
@@ -31,6 +34,19 @@ export class CheckInUseCase {
             throw new ResourceNotFoundError()
         }
 
+        const distance = getDistanceBetweenCoordinates(
+            {latitude: userLatitude, longitude: userLongitude},
+            {
+                latitude: gym.latitude.toNumber(), 
+                longitude: gym.longitude.toNumber(),
+            }
+        )
+
+        const MAX_DISTANCE_IN_KILOMETERS = 0.1
+        
+        if(distance > MAX_DISTANCE_IN_KILOMETERS) {
+            throw new Error()
+        }
         //CALCULATE DISTANCE BETTWEN USER AND GYM
 
         const checkInOnSameDay = await this.checkInsRepository.findByUserIdOnDate(
